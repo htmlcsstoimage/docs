e6d85431e4d0a77a839fcb959ed2d7150826325a
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2024-12-19 10:06:50 -0500

    move render when ready to parameters section

--- workers-site/index.js
+++ workers-site/index.js
@@ -29,73 +29,53 @@ async function redirectOrHandleEvent(event) {
   var path = url.pathname.replace(/\/$/, "");
   var site = "https://docs.htmlcsstoimage.com";
 
-  switch(path) {
+  switch (path) {
     case "/getting-started/creating-an-image":
       return Response.redirect(site + '/getting-started/using-the-api/', 301);
-      break;
+    case "/guies/render-when-reader":
+      return Response.redirect(site + '/parameters/render_when_ready', 301);
     case "/getting-started/deleting-an-image":
       return Response.redirect(site + '/getting-started/using-the-api/', 301);
-      break;
     case "/getting-started/retrieving-an-image":
       return Response.redirect(site + '/getting-started/using-the-api/', 301);
-      break;
     case "/getting-started/authentication":
       return Response.redirect(site + '/getting-started/using-the-api/', 301);
-      break;
     case "/getting-started/full-page-images":
       return Response.redirect(site + '/getting-started/convert-emails-images/', 301);
-      break;
     case "/getting-started/duplicate-image-detection":
       return Response.redirect(site + '/guides/duplicate-image-detection/', 301);
-      break;
     case "/getting-started/faq":
       return Response.redirect(site + '/faq', 301);
-      break;
     case "/advanced-examples/using-google-fonts":
       return Response.redirect(site + '/guides/using-google-fonts', 301);
-      break;
     case "/advanced-examples/rendering-emoji":
       return Response.redirect(site + '/guides/rendering-emoji', 301);
-      break;
     case "/advanced-examples/image-charts-with-highcharts":
       return Response.redirect(site + '/guides/image-charts-with-highchartsjs', 301);
-      break;
     case "/advanced-examples/transparent-background":
       return Response.redirect(site + '/guides/transparent-background', 301);
-      break;
     case "/advanced-examples/external-css-and-fonts":
       return Response.redirect(site + '/guides/external-css-js-and-fonts/', 301);
-      break;
     case "/getting-started/lossless-image-optimization":
       return Response.redirect(site + '/getting-started/file-formats', 301);
-      break;
     case "/advanced-examples/instagram-embed":
       return Response.redirect(site + '/guides/instagram-embed/', 301);
-      break;
     case "/advanced-examples/twitter-embed":
       return Response.redirect(site + '/guides/twitter-embed/', 301);
-      break;
     case "/getting-started/integromat-integration":
       return Response.redirect(site + '/integrations/integromat/', 301);
-      break;
     case "/integrations/integromat":
       return Response.redirect(site + '/integrations/make/', 301);
-      break;
     case "/getting-started/zapier-integration":
       return Response.redirect(site + '/integrations/zapier/', 301);
-      break;
     case "/getting-started/convert-emails-images":
       return Response.redirect(site + '/guides/convert-emails-images/', 301);
-      break;
     case "/getting-started/file-formats":
       return Response.redirect(site + '/guides/file-formats/', 301);
-      break;
     case "/guides/twitter-embed":
       return Response.redirect(site + '/guides/twitter-screenshot/', 301);
-      break;
     default:
       return await handleEvent(event);
-      break;
   }
 }
 
@@ -106,7 +86,7 @@ async function handleEvent(event) {
 
   if (event.request.url.match(/\.js$/) || event.request.url.match(/\.css$/) || event.request.url.match(/\.png$/)) {
     cacheControl = {
-      browserTTL: 365* 60 * 60 * 24,
+      browserTTL: 365 * 60 * 60 * 24,
       edgeTTL: 30 * 60 * 60 * 24,
       bypassCache: false, // do not bypass Cloudflare's cache
     }
@@ -155,7 +135,7 @@ async function handleEvent(event) {
         })
 
         return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
-      } catch (e) {}
+      } catch (e) { }
     }
 
     return new Response(e.message || e.toString(), { status: 500 })
